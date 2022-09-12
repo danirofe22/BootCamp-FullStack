db84ac42d9b2dcc02aeb8bc0adb116ee14e6485f
danirofe <danii.roofe22@gmail.com>
2022-09-12 19:30:29 +0200

    marcador con info

--- parte1/marcador/src/App.js
+++ parte1/marcador/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
-function App() {
+const Button = ({handleClick, text}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className='btn' onClick={handleClick}>{text}</button>
+    </>
+  )
+}
+const App =() => {
+  const [local, setLocal] = useState(0)
+  const [visitante, setVisitante] = useState(0)
+
+  const reiniciarMarcador = ({local, visitante}) => {
+    setLocal(0)
+    setVisitante(0)
+  }
+
+  const sumarLocal = () => {
+    
+  }
+
+  return (
+    <>
+      <div className='marcador'>
+        <div className='marcador-individual'>
+        <h3>LOCAL</h3>
+          <h1>{local}</h1>
+          <Button handleClick={() => setLocal(local + 1)} text=" + 1 "></Button>
+          <Button handleClick={() => setLocal(local - 1)} text=" - 1 "></Button>
+        </div>
+        <div className='marcador-division'>
+          <h3>VS</h3>
+        </div>
+        <div className='marcador-individual'>
+          <h3>VISITANTE</h3>
+          <h1>{visitante}</h1>
+          <Button handleClick={() => setVisitante(visitante + 1)} text=" + 1 "></Button>
+          <Button handleClick={() => setVisitante(visitante - 1)} text=" - 1 "></Button>
+        </div>
+        
+      </div>
+      <div className='btn-reiniciar'>
+        <Button handleClick={reiniciarMarcador} text = "Reiniciar Marcador"></Button>
+      </div>
+    </>
   );
 }
 
